ee88434e7b49c26fe9d158a1b3d62e63b54ff0b2
Sybee <imsybee@gmail.com>
2021-02-27 00:38:46 +0300

    upd

--- services/api/apiRequest.js
+++ services/api/apiRequest.js
@@ -6,14 +6,4 @@ const instance = axios.create({
   // headers: [],
 })
 
-// instance.interceptors.response.use(function (response) {
-//   console.log(response)
-//   return response;
-// }, function (error) {
-//   alert(error)
-//   return Promise.reject(error);
-// });
-
-// instance.defaults.headers.common['Authorization'] = AUTH_TOKEN;  EXAMPLE HOW TO INJECT HEADERS TO AXIOS INSTANCE LIKE INTERCEPTOR
-
 export default instance
